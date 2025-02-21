62f12cbe441a2f569b45bd5f95b60a945d366052
Dev Mittal <mittal.dev2002@gmail.com>
2025-02-21 16:06:37 +0530

    fix style about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -36,9 +36,9 @@ function About() {
             className="max-h-96 w-full object-cover rounded-xl"
           />
         </div>
-        <div className="w-3/4 p-2 pt-0 text-center md:text-left">
+        <div className="sm:w-3/4 p-2 pt-0 text-center md:text-center">
           <h2 className="text-4xl capitalize font-Rubik font-semibold md:text-right mb-4 text-black dark:text-white">My Achievements</h2>
-          <ul className="text-left font-sans pt-2 gap-1 list-disc text-xl text-gray-800 dark:text-gray-300">
+          <ul className="text-left ml-4 sm:ml-0 font-sans pt-2 gap-1 list-disc text-xl text-gray-800 dark:text-gray-300">
             <li className="my-1">Headed Events and PR team of our college's annual cultural fest - Vivacity.</li>
             <li className="my-1">Achieved distinction in Trinity College London's Grade 3 exam in the "Electronic Keyboard" category.</li>
             <li className="my-1">Received a 100% scholarship for the certification course by OEMs like Adobe, Apple, WhiteHat, etc. through RCAT Quizzathon-4 in Jaipur.</li>
